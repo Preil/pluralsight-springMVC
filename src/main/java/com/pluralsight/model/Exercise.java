3fa77a954a51f2398ce7f87d1f397e17bd3cf90b
Ilya Preobrazhenskiy <preil2000@gmail.com>
2017-06-21 17:12:44 +0300

    Add model class Exercise.

---
+++ src/main/java/com/pluralsight/model/Exercise.java
@@ -0,0 +1,16 @@
+package com.pluralsight.model;
+
+/**
+ * Ilya 21.06.2017.
+ */
+public class Exercise {
+    private int minutes;
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+}
