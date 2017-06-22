8dca4a460f94f8b82c74e6860de42a2d558f3d60
Ilya Preobrazhenskiy <preil2000@gmail.com>
2017-06-22 07:30:44 +0300

    Controller example with forward request.

--- src/main/java/com/pluralsight/controller/AddMinutes.java
+++ src/main/java/com/pluralsight/controller/AddMinutes.java
@@ -13,7 +13,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AddMinutes {
     @RequestMapping(value = "/addMinutes")
     public String addMinutes(@ModelAttribute("exercise") Exercise exercise) {
-        System.out.println("Exercise:" + exercise.getMinutes());
+        System.out.println("Exercise: " + exercise.getMinutes());
+        return "forward:addMoreMinutes.html";
+    }
+
+    @RequestMapping(value="/addMoreMinutes")
+    public String addMoreMinutes(@ModelAttribute("exercise")Exercise exercise){
+        System.out.println("Exercising: "+exercise.getMinutes());
         return "addMinutes";
     }
 }
