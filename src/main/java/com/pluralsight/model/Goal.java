aad56ee36f2f821583c6dd92fdde343eebfbd7bf
Ilya Preobrazhenskiy <preil2000@gmail.com>
2017-07-13 16:27:42 +0300

    Entity annotation example.

--- src/main/java/com/pluralsight/model/Goal.java
+++ src/main/java/com/pluralsight/model/Goal.java
@@ -2,12 +2,22 @@ package com.pluralsight.model;
 
 import org.hibernate.validator.constraints.Range;
 
+import javax.persistence.*;
+
 /**
  * Ilya 22.06.2017.
  */
-
+@Entity
+@Table(name = "goals")
 public class Goal {
+
+    @Id
+    @GeneratedValue
+    @Column(name="GOAL_ID")
+    private Long goalId;
+
     @Range(min=1, max=120)
+    @Column(name = "MINUTES")
     private int minutes;
 
     public int getMinutes() {
@@ -16,5 +26,13 @@ public class Goal {
 
     public void setMinutes(int minutes) {
         this.minutes = minutes;
+    }
+
+    public Long getGoalId() {
+        return goalId;
+    }
+
+    public void setGoalId(Long goalId) {
+        this.goalId = goalId;
     }
 }
