05cad578da2512590b1a2fd9b286a9133065470b
Ilya Preobrazhenskiy <preil2000@gmail.com>
2017-07-21 10:19:03 +0300

    Goal updates example using Jpa merge method.

--- src/main/java/com/pluralsight/repository/GoalRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/GoalRepositoryImpl.java
@@ -20,8 +20,12 @@ public class GoalRepositoryImpl implements GoalRepository {
 
     @Override
     public Goal save(Goal goal) {
-        em.persist(goal);
-        em.flush();
+        if (goal.getGoalId() == null){
+            em.persist(goal);
+            em.flush();
+        } else {
+            goal = em.merge(goal);
+        }
         return goal;
     }
 
